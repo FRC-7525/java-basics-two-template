3c58f090200ed076e7d5688f83dcc72f17e8ac41
nkalupahana <avishkank@gmail.com>
2024-09-19 23:28:18 -0700

    Ready to go

---
+++ app/src/main/java/conv/Converter.java
@@ -0,0 +1,61 @@
+package conv;
+
+import java.util.List;
+
+public class Converter {
+    // Numbers
+    public int add(int a, int b) {
+        // Add a and b together
+        return 0;
+    }
+
+    public int addPositive(int a, int b, int c) {
+        // Add a, b and c together, but don't add them to the total if they're negative
+        return 0;
+    }
+
+    public int addList(List<Integer> list) {
+        // Add all numbers in list together, using a for loop
+        return 0;
+    }
+
+    // Strings
+    public int getStringLength(String s) {
+        // Return the length of the string
+        return 0;
+    }
+
+    public String sign(int a) {
+        // Return "positive" if a is positive, "negative" if a is negative, and "zero" if a is zero
+        return "";
+    }
+
+    public int countHellos(List<String> list) {
+        // Count how many times the word "hello" appears in the list
+        return 0;
+    }
+
+
+    // Boolean
+    public boolean invert(boolean b) {
+        // Return the opposite of b (if true return false, if false return true)
+        // You should be able to do it in only one line of code!
+        return false;
+    }
+
+    public boolean isEven(int a) {
+        // Return true if a is even, false if a is odd
+        return false;
+    }
+
+    // Numbers (more complex)
+    public int addListWhile(List<Integer> list) {
+        // Add all numbers in list together, using a while loop
+        return 0;
+    }
+
+    public int addPositiveList(List<Integer> list) {
+        // Add only positive numbers in list together (ignore negative numbers)
+        return 0;
+    }
+}
